f59bd14361c176609cff5b8b634065ec9921b793
stressedball <tserchesi@gmail.com>
2022-11-13 22:26:04 +0100

    Webpacking

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+const body = document.querySelector('body');
+
+const mainContainer =  document.createElement('div');
+mainContainer.classList.add('main-container');
+body.appendChild(mainContainer);
