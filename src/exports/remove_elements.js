c15b3508cf64e2524d8206c148490a75c98be6a4
stressedball <tserchesi@gmail.com>
2022-11-27 17:59:49 +0100

    .

--- src/exports/remove_elements.js
+++
@@ -1,8 +0,0 @@
-function removeElements() {
-    const content = document.querySelectorAll('div#content div');
-    for (let element of content) {
-        element.remove();
-    }
-}
-
-export { removeElements };
